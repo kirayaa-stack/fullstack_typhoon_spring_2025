ada74122f128c347500c48732b17cc83ef4be75e
kira <ochirbaturantugs@gmail.com>
2025-03-27 17:53:44 +0800

    add: session 02

---
+++ week_01/session_02/introduce.py
@@ -0,0 +1,11 @@
+# introduce myself -- this is comment
+# introduce yourself in three lines
+# for example: your first name, lastname, age, gender
+# and occupation and hobbies
+print("My name is Ochirbat Urantugs")
+print("I am 22 years old")
+print("Born as a woman")
+print("i love to play different kind of games such as computer and physical ones like volley, basketball")
+print("I am university student and im in 4th year")
+print("I wrote it from Nano Editor")
+
